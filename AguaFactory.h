bfc2912e5e71f565c9c74946291758241274ab7c
Sebas <sbastiansalazar10@gmail.com>
2025-06-13 00:37:35 -0600

    cambios

--- AguaFactory.h
+++ AguaFactory.h
@@ -1,4 +1,21 @@
-#pragma once
+/* -------------------------------------------------------------------+
+* |
+* (c) 2025 |
+* EIF204 - Programación 2 |
+* 1er ciclo 2025 |
+* NRC 9999 – Grupo 00 |
+* Proyecto 2 |
+* |
+* 1-1977-0345; Menjívar Ramírez, Naara |
+* 1-1905-0975; Chaves Salazar, Sebastián |
+* A-0015-0163; Briones Rocha, Jordan |
+* |
+* versión 1.0.0 2025-06-01 |
+* |
+* -------------------------------------------------------------------+
+*/
+#ifndef AGUAFACTORY_H
+#define AGUAFACTORY_H
 #include "RecursoFactory.h"
 #include "Agua.h"
 
@@ -9,3 +26,4 @@ public:
     string getTipoFactory() const override;
 };
 
+#endif
--- CarnivoroFactory.h
+++ CarnivoroFactory.h
@@ -1,4 +1,21 @@
-#pragma once
+/* -------------------------------------------------------------------+
+* |
+* (c) 2025 |
+* EIF204 - Programación 2 |
+* 1er ciclo 2025 |
+* NRC 9999 – Grupo 00 |
+* Proyecto 2 |
+* |
+* 1-1977-0345; Menjívar Ramírez, Naara |
+* 1-1905-0975; Chaves Salazar, Sebastián |
+* A-0015-0163; Briones Rocha, Jordan |
+* |
+* versión 1.0.0 2025-06-01 |
+* |
+* -------------------------------------------------------------------+
+*/
+#ifndef CARNIVOROFACTORY_H
+#define CARNIVOROFACTORY_H
 #include "CriaturaFactory.h"
 #include "Carnivoro.h"
 
@@ -7,3 +24,5 @@ public:
     Criatura* crearCriatura(int x, int y, int energia, Ecosistema* eco, char clima);
     string getTipoFactory() const override;
 };
+
+#endif
